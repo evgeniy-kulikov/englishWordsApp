d2e2629ef6db14e5ee2c50c8b536b0f4eb2dc9d2
evgeniy-kulikov <redstepway@gmail.com>
2024-09-19 11:31:32 +0300

    lesson 09 logic of word learning var.1

--- app/src/main/java/ru/eugenyPets/englishWordsApp/MainActivity.kt
+++ app/src/main/java/ru/eugenyPets/englishWordsApp/MainActivity.kt
@@ -2,6 +2,8 @@ package ru.eugenyPets.englishWordsApp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.core.content.ContextCompat
 import androidx.core.view.isVisible
 import ru.eugenyPets.englishWordsApp.databinding.ActivityLearmWordBinding
@@ -26,80 +28,94 @@ class MainActivity : AppCompatActivity() {
 
         // нейтральный выбор
         binding.btnContinue.setOnClickListener {
-            markAnswerNeutral()
+            markAnswerNeutral(
+                binding.layoutAnswer1,
+                binding.tvVariantNumber1,
+                binding.tvVariantValue1,
+                )
+
+            markAnswerNeutral(
+                binding.layoutAnswer3,
+                binding.tvVariantNumber3,
+                binding.tvVariantValue3,
+            )
         }
 
 
         // корректный выбор
         binding.layoutAnswer3.setOnClickListener {
-//            it.isVisible = false
-            markAnswerCorrect()
+            markAnswerCorrect(
+                binding.layoutAnswer3,
+                binding.tvVariantNumber3,
+                binding.tvVariantValue3,
+            )
         }
 
         // некорректный выбор
         binding.layoutAnswer1.setOnClickListener {
-            markAnswerWrong()
+            markAnswerWrong(
+                binding.layoutAnswer1,
+                binding.tvVariantNumber1,
+                binding.tvVariantValue1,
+            )
         }
 
     }
 
     // функция сброса ответа и перехода к следующиму выбору
-    private fun markAnswerNeutral() {
-        with(binding) {
-            for (layout in listOf(layoutAnswer1, layoutAnswer3)) {
-                layout.background = ContextCompat.getDrawable(
+    private fun markAnswerNeutral(
+        layoutAnswer: LinearLayout,
+        tvVariantNumber: TextView,
+        tvVariantValue: TextView,
+        ) {
+
+        layoutAnswer.background = ContextCompat.getDrawable(
+            this@MainActivity,
+            R.drawable.shape_rounded_container
+            )
+
+        // функция apply {} для переменной textView производит сразу несколько действий
+        tvVariantValue.apply {
+            background = ContextCompat.getDrawable(
+                this@MainActivity,
+                R.drawable.shape_rounded_variants,
+            )
+            setTextColor(
+                ContextCompat.getColor(
                     this@MainActivity,
-                    R.drawable.shape_rounded_container
+                    R.color.textVariantsColor,
                 )
-            }
-
-            for (textView in listOf(tvVariantValue1, tvVariantValue3)) {
-                textView.setTextColor(
-                    ContextCompat.getColor(
-                        this@MainActivity,
-                        R.color.textVariantsColor
-                    )
-                )
-            }
-
-            // функция apply {} для переменной textView производит сразу несколько действий
-            for (textView in listOf(tvVariantNumber1, tvVariantNumber3)) {
-                textView.apply {
-                    background = ContextCompat.getDrawable(
-                        this@MainActivity,
-                        R.drawable.shape_rounded_variants,
-                    )
-                    setTextColor(
-                        ContextCompat.getColor(
-                            this@MainActivity,
-                            R.color.textVariantsColor,
-                        )
-                    )
-                }
-            }
-
-            layoutResult.isVisible = false
-            btnSkip.isVisible = true
-
+            )
         }
-    }
+
+        tvVariantNumber.setTextColor(
+            ContextCompat.getColor(
+                this@MainActivity,
+                R.color.textVariantsColor
+                )
+            )
+        }
 
 
     // функция неправильного ответа
-    private fun markAnswerWrong() {
+    private fun markAnswerWrong(
+        layoutAnswer: LinearLayout,
+        tvVariantNumber: TextView,
+        tvVariantValue: TextView,
+    ) {
 //        фон всей ячейки
-        binding.layoutAnswer1.background = ContextCompat.getDrawable(
+        layoutAnswer.background = ContextCompat.getDrawable(
             this@MainActivity,
             R.drawable.shape_rounded_containers_wrong
         )
 //      фон номера ячейки
-        binding.tvVariantNumber1.background = ContextCompat.getDrawable(
+        tvVariantNumber.background = ContextCompat.getDrawable(
             this@MainActivity,
             R.drawable.shape_rounded_variants_wrong
         )
 
-//      фон номера
-        binding.tvVariantNumber1.setTextColor(
+//      фон цифры номера
+        tvVariantNumber.setTextColor(
             ContextCompat.getColor(
                 this@MainActivity,
                 R.color.white
@@ -107,103 +123,48 @@ class MainActivity : AppCompatActivity() {
         )
 
 //      фон слова
-        binding.tvVariantValue1.setTextColor(
+        tvVariantValue.setTextColor(
             ContextCompat.getColor(
                 this@MainActivity,
                 R.color.wrongAnswerColor
             )
         )
-
-        //      Button Continue
-        binding.btnSkip.isVisible = false
-
-        binding.layoutResult.setBackgroundColor(
-            ContextCompat.getColor(
-                this@MainActivity,
-                R.color.wrongAnswerColor
-            )
-        )
-
-        binding.ivResultIcon.setImageDrawable(
-            ContextCompat.getDrawable(
-                this@MainActivity,
-                R.drawable.ic_wrong
-            )
-        )
-
-        binding.tvResultMessage.text = resources.getString(R.string.title_wrong)
-
-        binding.btnContinue.setTextColor(
-            ContextCompat.getColor(
-                this@MainActivity,
-                R.color.wrongAnswerColor
-            )
-        )
-
-//      включаем видимость после всех действий (для исключения мерцаний элементов на экране)
-        binding.layoutResult.isVisible = true
-
     }
 
 
-
     // функция верного ответа
-    private fun markAnswerCorrect() {
-//      фон всей ячейки
-        binding.layoutAnswer3.background = ContextCompat.getDrawable(
+    private fun markAnswerCorrect(
+        layoutAnswer: LinearLayout,
+        tvVariantNumber: TextView,
+        tvVariantValue: TextView,
+    ) {
+        // фон всей ячейки
+        layoutAnswer.background = ContextCompat.getDrawable(
             this@MainActivity,
             R.drawable.shape_rounded_containers_correct
         )
-//      фон номера ячейки
-        binding.tvVariantNumber3.background = ContextCompat.getDrawable(
+        // фон номера ячейки
+        tvVariantNumber.background = ContextCompat.getDrawable(
             this@MainActivity,
             R.drawable.shape_rounded_variants_correct
         )
 
-//      фон номера
-        binding.tvVariantNumber3.setTextColor(
+        // фон номера
+        tvVariantNumber.setTextColor(
             ContextCompat.getColor(
                 this@MainActivity,
                 R.color.white
             )
         )
 
-//      фон слова
-        binding.tvVariantValue3.setTextColor(
+        //  цвет текста слова
+        tvVariantValue.setTextColor(
             ContextCompat.getColor(
                 this@MainActivity,
                 R.color.correctAnswerColor
             )
         )
 
-        //      Button Continue
-        binding.btnSkip.isVisible = false
-
-        binding.layoutResult.setBackgroundColor(
-            ContextCompat.getColor(
-                this@MainActivity,
-                R.color.correctAnswerColor
-            )
-        )
-
-        binding.ivResultIcon.setImageDrawable(
-            ContextCompat.getDrawable(
-                this@MainActivity,
-                R.drawable.ic_correct
-            )
-        )
-
-        binding.tvResultMessage.text = resources.getString(R.string.title_correct)
-
-        binding.btnContinue.setTextColor(
-            ContextCompat.getColor(
-                this@MainActivity,
-                R.color.correctAnswerColor
-            )
-        )
-
-//      включаем видимость после всех действий (для исключения мерцаний элементов на экране)
-        binding.layoutResult.isVisible = true
 
     }
 }
