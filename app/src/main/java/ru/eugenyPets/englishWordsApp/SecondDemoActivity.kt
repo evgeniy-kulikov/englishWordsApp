b5600d611f148b621bae1c79e03a2262858c0883
evgeniy-kulikov <redstepway@gmail.com>
2024-09-26 18:47:13 +0300

    second & first activity work

--- app/src/main/java/ru/eugenyPets/englishWordsApp/SecondDemoActivity.kt
+++ app/src/main/java/ru/eugenyPets/englishWordsApp/SecondDemoActivity.kt
@@ -3,21 +3,21 @@ package ru.eugenyPets.englishWordsApp
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import ru.eugenyPets.englishWordsApp.databinding.ActivityFitstDemoBinding
+import ru.eugenyPets.englishWordsApp.databinding.ActivitySecondDemoBinding
 
-class FirstDemoActivity: AppCompatActivity() {
+class SecondDemoActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityFitstDemoBinding
+    private lateinit var binding: ActivitySecondDemoBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityFitstDemoBinding.inflate(layoutInflater)
+        binding = ActivitySecondDemoBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         with(binding) {
-            btnOpenSecond.setOnClickListener {
+            btnOpenFirst.setOnClickListener {
                 // объявляем Intent (намерение) откуда - куда
-                val intent = Intent(this@FirstDemoActivity, SecondDemoActivity::class.java)
+                val intent = Intent(this@SecondDemoActivity, FirstDemoActivity::class.java)
                 startActivity(intent)  // запуск намерения
             }
         }
